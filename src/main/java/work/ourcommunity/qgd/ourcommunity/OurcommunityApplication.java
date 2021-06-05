671076b42c64bc197e16958a667cb4bb0d991d0a
Codingxiaoxi <laotianzc@outlook.com>
2021-06-05 13:58:28 +0800

    first commit

---
+++ src/main/java/work/ourcommunity/qgd/ourcommunity/OurcommunityApplication.java
@@ -0,0 +1,13 @@
+package work.ourcommunity.qgd.ourcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/work/ourcommunity/qgd/ourcommunity/OurcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package work.ourcommunity.qgd.ourcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
